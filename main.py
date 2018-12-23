66570561808e6fccdb5aa9a281e8bea5e672665d
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-12-23 10:46:43 +0700

    add evaluation func

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import matplotlib
+import math
 from typing import List, Tuple
 
 class City (object):
-    def __init__(self, name: str, coordinate: List[float]):
+    def __init__(self, name: str, x: float, y: float):
         self.name = name
-        self.coordinate = coordinate
+        self.x = x
+        self.y = y
 
     def __repr__(self):
-        return f'name: {self.name}, coordinate: {self.coordinate}'
+        return f'name: {self.name}, coordinate: {self.x},{self.y}'
 
-def evaluate_distance(a: City, b : City):
-    pass
+def evaluate_distance(a: City, b : City) -> float:
+    return math.sqrt(abs(a.x - b.x) + abs(a.y - b.y))
 
 def read_data(file_location):
     with open(file_location) as file: # Use file to refer to the file object
@@ -21,8 +23,8 @@ def read_data(file_location):
             cities = list()
             for name in names:
                 line = file.readline().split(',')
-                coordinate = list(map(lambda x: float(x), line))
-                cities.append(City(name, coordinate))
+                x, y = list(map(lambda x: float(x), line))
+                cities.append(City(name, x, y))
             optimal = float(file.readline())
             test_case.append((cities, optimal))
         
